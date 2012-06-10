984214a6002bd53c9cdeed74eb92d13e0634ecbc
Laurian Gridinoc <laurian@gridinoc.name>
2012-06-09 21:54:27 -0500

    String.prototype.trim
    String.prototype.trim polyfill from https://gist.github.com/1035982

--- raphael-svg-import.js
+++ raphael-svg-import.js
@@ -18,7 +18,14 @@
 * Modifications by Laurian Gridinoc
 * - commented out the try/catch error alert block
 * - added a callback to which each svg element / raphael shape pair is sent
+* - added the String.prototype.trim polyfill from https://gist.github.com/1035982
 */
+
+''.trim || (String.prototype.trim = // Use the native method if available, otherwise define a polyfill:
+  function () { // trim returns a new string (which replace supports)
+    return this.replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g,'') // trim the left and right sides of the string
+  });
+
 Raphael.fn.importSVG = function (svgXML, callback) {
   // try {
     
